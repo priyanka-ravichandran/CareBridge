1fe16db523d3e88989b6c380f388ffe94ba1c92b
unknown <pr397332@dal.ca>
2023-10-24 18:59:08 -0300

    login and signup with OAuth

---
+++ CareBridgeUi/app/components/SignupScreen.js
@@ -0,0 +1,163 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import * as AuthSession from 'expo-auth-session';
+
+const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
+
+  const [request, response, promptAsync] = AuthSession.useAuthRequest({
+    clientId: 'http://1071408551074-7h7t6vcthqqlr13s3m6fibff3lvp6d0u.apps.googleusercontent.com/',
+    redirectUri: AuthSession.makeRedirectUri(),
+    scopes: ['profile', 'email'],
+    responseType: 'token',
+    usePKCE: false,
+  }, {
+    authorizationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
+    tokenEndpoint: 'https://oauth2.googleapis.com/token',
+    revocationEndpoint: 'https://oauth2.googleapis.com/revoke',
+  });
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      console.log(response.params.access_token);
+    }
+  }, [response]);
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email) {
+      setEmailError("Email is required.");
+      return false;
+    } else if (!emailRegex.test(email)) {
+      setEmailError("Invalid email address.");
+      return false;
+    } else {
+      setEmailError(null);
+      return true;
+    }
+  };
+
+  const validatePassword = () => {
+    if (!password) {
+      setPasswordError("Password is required.");
+      return false;
+    } else if (password.length < 6) {
+      setPasswordError("Password must be at least 6 characters long.");
+      return false;
+    } else {
+      setPasswordError(null);
+      return true;
+    }
+  };
+
+  const handleSignup = () => {
+    const isEmailValid = validateEmail();
+    const isPasswordValid = validatePassword();
+
+    if (isEmailValid && isPasswordValid) {
+      // Your signup logic here
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Signup</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        onBlur={validateEmail}
+      />
+      {emailError && <Text style={styles.errorText}>{emailError}</Text>}
+
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        onBlur={validatePassword}
+        secureTextEntry
+      />
+      {passwordError && <Text style={styles.errorText}>{passwordError}</Text>}
+
+      <TouchableOpacity style={styles.signupButton} onPress={handleSignup}>
+        <Text style={styles.signupText}>Signup</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.googleButton} onPress={() => promptAsync()}>
+        <Text style={styles.googleButtonText}>Signup with Google</Text>
+      </TouchableOpacity>
+
+      <View style={styles.row}>
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <Text style={styles.loginText}>Already have an account? Login</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    backgroundColor: "#E6E6E6",
+    marginVertical: 10,
+    paddingHorizontal: 10,
+  },
+  signupButton: {
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+    width: '80%',
+  },
+  signupText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  googleButton: {
+    backgroundColor: '#db4437',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+    width: '80%',
+  },
+  googleButtonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    marginTop: 10,
+  },
+  loginText: {
+    color: 'black',
+    textDecorationLine: 'underline',
+  },
+  errorText: {
+    color: 'red',
+    textAlign: 'left',
+    width: '80%',
+    marginTop: 5,
+  },
+});
+
+export default SignupScreen;
