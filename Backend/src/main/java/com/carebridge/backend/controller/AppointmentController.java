2c45cdfe314b060b1d1ec811515b0dcb70172e4a
yuci <yc457137@dal.ca>
2023-11-11 17:31:00 -0400

    Refactored Appointment backend for #59

--- Backend/src/main/java/com/carebridge/backend/controller/AppointmentController.java
+++ Backend/src/main/java/com/carebridge/backend/controller/AppointmentController.java
@@ -2,60 +2,50 @@ package com.carebridge.backend.controller;
 
 
 import com.carebridge.backend.entity.Appointment;
-import com.carebridge.backend.entity.AppointmentList;
-import com.carebridge.backend.service.AppointmentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.carebridge.backend.repo.AppointmentRepository;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("api/v1/appointment")
 public class AppointmentController {
-private AppointmentService appointmentService;
-private AppointmentList appointmentList;
-@Autowired
-public  AppointmentController(AppointmentService appointmentService){
-    this.appointmentService=appointmentService;
+private final AppointmentRepository appointmentRepository;
+public  AppointmentController(AppointmentRepository appointmentRepository){
+    this.appointmentRepository=appointmentRepository;
 }
-@RequestMapping(value = "/seeAppointmentFamily" ,method = RequestMethod.GET)
-    public ResponseEntity<String> seeAppointmentFamily(@RequestParam String userID){
-    appointmentList=appointmentService.fetchAppointmentFamily(userID);
-    System.out.println("Showing the appointment");
-    return ResponseEntity.ok("Showing the appointment");
+@GetMapping( "/Appointment")
+    List<Appointment> all(){
+    return appointmentRepository.findAll();
 }
-@RequestMapping(value = "/seeAppointmentElderly", method = RequestMethod.GET)
-    public ResponseEntity<String> seeAppointmentElderly(@RequestParam String elderlyID){
-    List<Appointment> appointmentList1=appointmentService.fetchAppointmentSenior(elderlyID);
-    System.out.println("Showing the appointment");
-    return ResponseEntity.ok("Showing the appointment");
+@GetMapping("/Appointment/elderly")
+    List<Appointment> seeAppointmentElderly(@RequestParam int elderlyID){
+    return appointmentRepository.findAppointmentByElderlyId(elderlyID);
 }
-@RequestMapping(value = "/addAppointment", method = RequestMethod.GET)
-    public ResponseEntity<String> addAppointment(@RequestParam Appointment appointment){
-    boolean res=true;
-    res=appointmentService.addAppointment(appointment);
-    if(res==true)
-    {
-        System.out.println("added successful");
-        return ResponseEntity.ok("added successful");
-    }
-    return ResponseEntity.ok("error adding");
+@GetMapping("/Appointment/family")
+List<Appointment> seeAppointmentFamily(@RequestParam int familyMemberId){
+    return appointmentRepository.findAppointmentByFamilyMemberId(familyMemberId);
+}
+@PostMapping("/Appointment")
+    Appointment addAppointment(@RequestParam Appointment appointment){
+    return appointmentRepository.save(appointment);
 
 }
-    @RequestMapping(value = "/editAppointment", method = RequestMethod.GET)
-    public ResponseEntity<String> editAppointment(@RequestParam Appointment appointment){
-        boolean res=true;
-        res=appointmentService.editAppointment(appointment);
-        if(res==true)
-        {
-            System.out.println("edited successful");
-            return ResponseEntity.ok("edited successful");
-        }
-        return ResponseEntity.ok("error editing");
+    @PutMapping("/Appointment")
+    Optional<Appointment> editAppointment(@RequestParam Appointment newAppointment,@RequestParam int familyMemberId
+    ,@RequestParam int elderlyId){
+        return appointmentRepository.findAppointmentByFamilyMemberIdAndElderlyId(familyMemberId,elderlyId)
+                .map(
+                        appointment -> {
+                            appointment.setElderlyId(newAppointment.getElderlyId());
+                            appointment.setFamilyMemberId(newAppointment.getFamilyMemberId());
+                            appointment.setAddress(newAppointment.getAddress());
+                            appointment.setTime(newAppointment.getTime());
+                            appointment.setRecurring(newAppointment.isRecurring());
+                            appointment.setBufferTime(newAppointment.getBufferTime());
+                            return appointmentRepository.save(appointment);
+                        }
+                );
 
     }
 }
