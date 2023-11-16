6682b3e7e70c45724ee2c301b534eefdb0ce8a6e
unknown <pr397332@dal.ca>
2023-11-15 22:30:59 -0400

    fixed the app logo bug

--- CareBridgeUi/app/components/authentication/LandingScreen.js
+++ CareBridgeUi/app/components/authentication/LandingScreen.js
@@ -4,7 +4,7 @@ import { View, Text, Image, StyleSheet, Pressable } from "react-native";
 const LandingScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Image source={require("../../assets/appLogo.png")} style={styles.logo} />
+      <Image source={require("../../assets/appLogo.jpeg")} style={styles.logo} />
       <Pressable
         style={styles.pressableStyle}
         title="Login"
