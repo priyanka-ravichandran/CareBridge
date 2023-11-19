54d18a92564fcbe984fddfecfb3e4083eefca58e
Aditya Pattani <ad943407@dal.ca>
2023-11-19 00:44:55 -0400

    Added a class for Reminder

---
+++ Backend/src/main/java/com/carebridge/backend/controller/ReminderController.java
@@ -0,0 +1,63 @@
+package com.carebridge.backend.controller;
+
+import com.carebridge.backend.entity.Reminder;
+import com.carebridge.backend.repo.ReminderRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class ReminderController {
+    private final ReminderRepository reminderRepository;
+
+    public ReminderController(ReminderRepository reminderRepository) {
+        this.reminderRepository = reminderRepository;
+    }
+
+    @PostMapping("/reminder")
+    @CrossOrigin(origins = "*")
+    Reminder reminder(@RequestBody Reminder reminder) {
+        return reminderRepository.save(reminder);
+    }
+
+    @GetMapping("/reminder")
+    @CrossOrigin(origins = "*")
+    List<Reminder> all() {
+        return reminderRepository.findAll();
+    }
+
+    @GetMapping("/reminder/{elderlyId}")
+    @CrossOrigin(origins = "*")
+    List<Reminder> getRemindersForElderly(@PathVariable int elderlyId) {
+        return reminderRepository.getRemindersByElderlyId(elderlyId);
+    }
+
+    @GetMapping("/reminder/{elderlyId}/{day}")
+    @CrossOrigin(origins = "*")
+    List<Reminder> getRemindersForElderly(@PathVariable int elderlyId, @PathVariable String day) {
+        return reminderRepository.getRemindersByElderlyIdAndDay(elderlyId, day);
+    }
+
+    @PutMapping("/reminder/q")
+    @CrossOrigin(origins = "*")
+    Optional<Reminder> updateChecklistItem(@RequestBody Reminder newReminder,
+                                                   @RequestParam int elderlyId, @RequestParam int volunteerId,
+                                                   @RequestParam String reminderNumber) {
+        return reminderRepository.getRemindersByElderlyIdAndVolunteerIdAndReminderNumber(elderlyId, volunteerId, reminderNumber)
+                .map(reminder -> {
+                    reminder.setDescription(newReminder.getDescription());
+                    reminder.setDay(newReminder.getDay());
+                    reminder.setTime(newReminder.getTime());
+                    return reminderRepository.save(reminder);
+                });
+    }
+
+    @Transactional
+    @DeleteMapping("/reminder/q")
+    @CrossOrigin(origins = "*")
+    public void deleteReminder(@RequestParam String medicineReminderNumber) {
+        reminderRepository.deleteRemindersByReminderNumber(medicineReminderNumber);
+    }
+}
