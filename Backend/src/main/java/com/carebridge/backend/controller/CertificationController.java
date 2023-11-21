31c42b7da16bc7b7ddf8ff5ac97b461ebde956e7
yuci <yc457137@dal.ca>
2023-11-21 16:42:36 -0400

    Tested all controller for email function#73

--- Backend/src/main/java/com/carebridge/backend/controller/CertificationController.java
+++ Backend/src/main/java/com/carebridge/backend/controller/CertificationController.java
@@ -2,6 +2,7 @@ package com.carebridge.backend.controller;
 
 import com.carebridge.backend.entity.Certification;
 import com.carebridge.backend.repo.CertificationRepository;
+import com.carebridge.backend.service.EmailService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,14 +10,17 @@ import java.util.List;
 @RestController
 public class CertificationController {
     private final CertificationRepository certificationRepository;
+    private final EmailService emailService;
 
-    public CertificationController(CertificationRepository certificationRepository) {
+    public CertificationController(CertificationRepository certificationRepository, EmailService emailService) {
         this.certificationRepository = certificationRepository;
+        this.emailService = emailService;
     }
 
     @PostMapping("/certification")
     @CrossOrigin(origins = "*")
-    Certification certification(@RequestBody Certification certification) {
+    Certification certification(@RequestBody Certification certification,@RequestParam String userEmail) {
+        emailService.sendEmail(userEmail,"Add","Adding certificate was successful");
         return certificationRepository.save(certification);
     }
 
